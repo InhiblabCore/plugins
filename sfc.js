d41cf1e356409c620ef2eed3c3657d85abf97dac
inhiblab <101786977+inhiblab@users.noreply.github.com>
2022-11-26 18:20:56 +0800

    Create sfc.js

---
+++ sfc.js
@@ -0,0 +1,4 @@
+(function(){   
+   const dom =document.querySelector('nav').querySelector(".github").querySelector("a");
+   dom.setAttribute("href","https://github.com/InhiblabCore/vue-hooks-plus")    
+})();
